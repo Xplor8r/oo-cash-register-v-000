e79cc650fbfbff8cc4549bf86bfc45e2b39549db
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 04:36:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
       self.total += price
       @items << title
     end
-    self.last_transaction = 
+    self.last_transaction = amount * quantity
   end
   
   def apply_discount
